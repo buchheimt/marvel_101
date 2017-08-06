de5d76666745caac239a6bac1401888351042b6d
Tyler <tbuchhei@alumni.nd.edu>
2017-08-06 15:23:57 -0500

    add main menu

--- lib/marvel_101/cli.rb
+++ lib/marvel_101/cli.rb
@@ -1,7 +1,17 @@
 class Marvel101::CLI
 
   def call
-    puts "welcome to Marvel 101!"
+    puts "Welcome to Marvel 101!"
+    main_menu
+  end
+
+  def main_menu
+    puts "Let's get started. Enter a corresponding number from the options below:"
+    puts "1. Popular Teams!"
+    puts "2. Popular Heroes!"
+    puts "3. Popular Villains!"
+    puts "4. Marvel's Currently Featured!"
+    puts "5. Women of Marvel!"
   end
 
 end
