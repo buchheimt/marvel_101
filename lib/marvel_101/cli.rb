202d0ac70dbb652f0fdb1e4d5c4c11cabdc11d9c
Tyler <tbuchhei@alumni.nd.edu>
2017-08-06 15:42:38 -0500

    stub out main menu and create detail menu

--- lib/marvel_101/cli.rb
+++ lib/marvel_101/cli.rb
@@ -5,13 +5,35 @@ class Marvel101::CLI
     main_menu
   end
 
-  def main_menu
-    puts "Let's get started. Enter a corresponding number from the options below:"
+  def list_main_options
+    puts "Here are your primary options:"
     puts "1. Popular Teams!"
     puts "2. Popular Heroes!"
     puts "3. Popular Villains!"
     puts "4. Marvel's Currently Featured!"
     puts "5. Women of Marvel!"
+    puts "6. Exit Marvel 101"
+  end
+
+  def main_menu
+    list_main_options
+    puts "Select a number from the options above and we'll get started!"
+    input = gets.chomp.to_i
+    case input
+    when 1 then detail_menu("teams")
+    when 2 then detail_menu("heroes")
+    when 3 then detail_menu("villains")
+    when 4 then detail_menu("featured")
+    when 5 then detail_menu("women")
+    when 6 then puts "Sure thing, have a super day! (haha super... get it?)"
+    else
+      puts "Yeah... that's not an option. Let's try that again."
+      main_menu
+    end
+  end
+
+  def detail_menu(topic)
+    puts "cool you selected #{topic}"
   end
 
 end
