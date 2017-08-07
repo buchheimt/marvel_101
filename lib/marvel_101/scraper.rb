88525bbd516de61c07f04eba4fd72d148175a21e
Tyler <tbuchhei@alumni.nd.edu>
2017-08-07 18:29:54 -0500

    fix deleted #scrape_team code

--- lib/marvel_101/scraper.rb
+++ lib/marvel_101/scraper.rb
@@ -33,6 +33,12 @@ class Marvel101::Scraper
     details[:description] = description_scrape(doc) if description_scrape(doc)
 
     members = []
+    members_grid = doc.css("div.grid-container").first
+    members_grid.css("div.row-item").each do |card|
+      name = card.css("a.meta-title").text.strip
+      url = "http:#{card.css("a.meta-title").attr("href").value}"
+      members << Marvel101::Character.find_or_create_by_name(name, url)
+    end
     details[:members] = members
     url_101_text = doc.css("div#MarvelVideo101 script").text
     if url_101_text != ""
