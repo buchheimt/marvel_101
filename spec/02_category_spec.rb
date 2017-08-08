29f2daf5c612b46173ff3245bfa593a7f2b32bf6
Tyler <tbuchhei@alumni.nd.edu>
2017-08-07 19:58:39 -0500

    expand Team tests and setup fixtures file

--- spec/02_category_spec.rb
+++ spec/02_category_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "Marvel101::Category" do
 
   describe "#initialize" do
     it "Initializes a Category with a name"  do
-      new_category = Marvel101::Category.new("Teams", "marvel url")
+      new_category = Marvel101::Category.new("Teams", "http://marvel.com/characters/list/997/titanic_teams")
       expect(new_category.name).to eq("Teams")
     end
 
     it "Initializes a Category with a url"  do
-      new_category = Marvel101::Category.new("Teams", "marvel url")
+      new_category = Marvel101::Category.new("Teams", "http://marvel.com/characters/list/997/titanic_teams")
       expect(new_category.url).to eq("marvel url")
     end
   end
--- spec/03_team_spec.rb
+++ spec/03_team_spec.rb
@@ -2,24 +2,31 @@ require "spec_helper"
 
 RSpec.describe "Marvel101::Team" do
 
+  new_team = Marvel101::Team.new("Avengers", "http://marvel.com/characters/68/avengers")
+  new_team.get_info
+
   describe "#initialize" do
     it "Initializes a Team with a name"  do
-      new_team = Marvel101::Team.new("Avengers", "Avengers url")
       expect(new_team.name).to eq("Avengers")
     end
   end
 
   describe "#get_info" do
-    it "retrieves and sets an array of members to @members"  do
-      new_team = Marvel101::Team.new("Avengers", "Avengers url")
-      new_team.get_info
-      expect(new_team.members[0].name).to eq("Thor")
+    it "retrieves and sets an array of members if applicable"  do
+      expect(new_team.members[0].name).to eq("Black Panther")
     end
 
-    it "retrieves and mass assigns scraped attributes" do
-      new_team = Marvel101::Team.new("Avengers", "Avengers url")
-      new_team.get_info
-      expect(new_team.description).to eq("THE super team")
+    it "retrieves and assigns a description if applicable" do
+      description = "Earth's Mightiest Heroes joined forces to take on threats that were too big for any one hero to tackle. With a roster that has included Captain America, Iron Man, Ant-Man, Hulk, Thor, Wasp and dozens more over the years, the Avengers have come to be regarded as Earth's No. 1 team."
+      expect(new_team.description).to eq(description)
+    end
+
+    it "retrieves and sets a marvel 101 link applicable"  do
+      expect(new_team.url_101).to eq("https://www.youtube.com/watch?v=DLy08aZx5PQ")
+    end
+
+    it "retrieves and sets a marvel wiki link if applicable"  do
+      expect(new_team.url_wiki).to eq("http://marvel.com/universe/Avengers")
     end
   end
 
